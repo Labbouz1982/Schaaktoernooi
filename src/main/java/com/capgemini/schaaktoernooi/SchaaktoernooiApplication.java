c223d50370b0b73269282b8a5fe5cb39e37dd3c5
AhmedLabbouz <alabbouza@gmail.com>
2019-02-14 13:11:28 +0100

    start

---
+++ src/main/java/com/capgemini/schaaktoernooi/SchaaktoernooiApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.schaaktoernooi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchaaktoernooiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchaaktoernooiApplication.class, args);
+	}
+
+}
+
